2f0c49d065ed21851a352c122b8deaf7de9481de
Frog <shmily40686@gmail.com>
2020-04-01 14:17:13 -0700

    delete todo done

--- index.php
+++ index.php
@@ -2,7 +2,30 @@
     include "db.inc.php";
 
     $query = 'SELECT * FROM todo';
-    $res = mysqli_query($connection,$query)
+    $res = mysqli_query($connection,$query);
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $todo = $_POST['todo'];
+        $date = date('1 ds F\,Y');
+        $sql = "INSERT INTO todo(t_name,t_date) VALUE('$todo','$date');";
+        $result = mysqli_query($connection,$sql);
+        if(!$result) {
+            die("failed");
+        } else {
+            header("Location:index.php?todo-added");
+        }
+    }
+
+    if(isset($_GET["delete_todo"])) {
+        $id = $_GET["delete_todo"];
+        $sql_delete = "DELETE FROM todo WHERE t_id = $id"; 
+        $delete_result = mysqli_query($connection,$sql_delete);
+         if(!$delete_result) {
+            die("failed");
+        } else {
+            header("Location:index.php?todo-deleted");
+        }
+    }
 ?>
 
 
@@ -19,12 +42,16 @@
             <input type="submit" value="add todo" >
         </form>
         <ul>
-            <?=
+            <?php
               while($row = mysqli_fetch_assoc($res)) {
                   $t_name = $row['t_name'];
-              }
+                  $t_id = $row['t_id'];
             ?>
-            <li>eat food</li>
+            <li>
+                <?php echo $t_name; ?>
+                <a href="index.php?delete_todo=<?php echo $t_id; ?>">delete</a>
+            </li>
+            <?php } ?>
         </ul>
     </div>
     
